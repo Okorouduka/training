34d7bfa4399768cab81ad0296464d2552bdcf548
Okorouduka <okorouduka@yahoo.com>
2025-04-18 17:57:39 -0300

    Add files via upload

---
+++ ass1.js
@@ -0,0 +1,6 @@
+const m = (55.5);
+const initialTemperature = (3.5);
+const finalTemperature = (10.5);
+const energy = m ** (finalTemperature - initialTemperature) ** 4184;
+
+console.log ("theEnergyNeededIs" + energy)
